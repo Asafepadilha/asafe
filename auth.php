f3c0d11b2d6aa3908cd5ae266f6b2c4f52532906
Asafepadilha <asafepadilha05@gmail.com>
2025-06-26 22:03:28 -0300

    Create auth.php

---
+++ auth.php
@@ -0,0 +1,14 @@
+<div class="container">
+<link rel="stylesheet" href="assets/css/style.css">
+<?php
+function is_logged() {
+  return !empty($_SESSION['user_id']);
+}
+function require_login() {
+  if (!is_logged()) {
+    header('Location: login.php'); exit;
+  }
+}
+function current_user_id() {
+  return $_SESSION['user_id'] ?? null;
+}
